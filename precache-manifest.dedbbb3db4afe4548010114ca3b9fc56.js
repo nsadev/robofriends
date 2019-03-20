8da371fce9edd4a997649dc9bb21155ab9687eaa
nsadev <47278974+nsadev@users.noreply.github.com>
2019-03-20 19:18:48 +0100

    Updates

---
+++ precache-manifest.dedbbb3db4afe4548010114ca3b9fc56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aedb4e53e89ac656e8a6",
+    "url": "/robofriends/static/js/runtime~main.aedb4e53.js"
+  },
+  {
+    "revision": "828cdf111ac08e9721b2",
+    "url": "/robofriends/static/js/main.828cdf11.chunk.js"
+  },
+  {
+    "revision": "dc7beb70d521f4dc051a",
+    "url": "/robofriends/static/js/2.dc7beb70.chunk.js"
+  },
+  {
+    "revision": "828cdf111ac08e9721b2",
+    "url": "/robofriends/static/css/main.70957ab2.chunk.css"
+  },
+  {
+    "revision": "dc7beb70d521f4dc051a",
+    "url": "/robofriends/static/css/2.1847a606.chunk.css"
+  },
+  {
+    "revision": "f322edb97813a24062b443edcd066565",
+    "url": "/robofriends/index.html"
+  }
+];
